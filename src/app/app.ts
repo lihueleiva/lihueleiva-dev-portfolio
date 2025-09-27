53925987c8524e38fcc16db681fe7a6dce4a8fdc
Lihue Leiva <leivalihue@gmail.com>
2025-09-26 22:06:44 -0300

    feat: setup global styles, fonts, and bootstrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('lihueleiva-dev-portfolio');
+export class AppComponent {
 }
