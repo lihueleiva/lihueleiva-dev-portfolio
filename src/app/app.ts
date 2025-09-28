11d4306b3ab8d984b44fda2baeab394b8ab49164
Lihue Leiva <leivalihue@gmail.com>
2025-09-27 22:24:50 -0300

    feat: build about section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Header } from "./components/header/header";
 import { Hero } from "./components/hero/hero";
+import { About } from './components/about/about';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, Hero],
+  imports: [Header, Hero, About],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
