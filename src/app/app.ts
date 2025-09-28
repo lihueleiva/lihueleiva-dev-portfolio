d6044728bb7a50ef3cc58709f0fdb628393a8dba
Lihue Leiva <leivalihue@gmail.com>
2025-09-27 22:38:49 -0300

    feat: build skills section

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Header } from "./components/header/header";
 import { Hero } from "./components/hero/hero";
 import { About } from './components/about/about';
+import { Skills } from "./components/skills/skills";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, Hero, About],
+  imports: [Header, Hero, About, Skills],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
