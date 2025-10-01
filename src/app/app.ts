874218b9ea8f38bb5541875be786fc9b1fae7a27
Lihue Leiva <leivalihue@gmail.com>
2025-10-01 17:57:54 -0300

    refactor: implement devicon for skills section

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,17 +2,17 @@ import { Component } from '@angular/core';
 import { Header } from "./components/header/header";
 import { Hero } from "./components/hero/hero";
 import { About } from './components/about/about';
-import { Skills } from "./components/skills/skills";
+import { SkillsComponent } from "./components/skills/skills";
 import { ProjectsComponent } from './components/projects/projects';
 import { CertificationsComponent } from './components/certifications/certifications';
 import { Contact } from "./components/contact/contact";
-import { Footer } from "./components/footer/footer";
+import { FooterComponent } from "./components/footer/footer";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, Hero, About, Skills, ProjectsComponent, CertificationsComponent, Contact, Footer],
+  imports: [Header, Hero, About, SkillsComponent, ProjectsComponent, CertificationsComponent, Contact, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/components/skills/skills.ts
+++ src/app/components/skills/skills.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
+  standalone: true,
   imports: [],
   templateUrl: './skills.html',
   styleUrl: './skills.scss'
 })
-export class Skills {
-
+export class SkillsComponent {
 }
