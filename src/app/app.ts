3c981b3a75ab9c2c6ed9df6b9686ddf96ca58a4d
Lihue Leiva <leivalihue@gmail.com>
2025-10-03 00:12:11 -0300

    refactor: contact section

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,14 +5,14 @@ import { About } from './components/about/about';
 import { SkillsComponent } from "./components/skills/skills";
 import { ProjectsComponent } from './components/projects/projects';
 import { CertificationsComponent } from './components/certifications/certifications';
-import { Contact } from "./components/contact/contact";
+import { ContactComponent } from "./components/contact/contact";
 import { FooterComponent } from "./components/footer/footer";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, Hero, About, SkillsComponent, ProjectsComponent, CertificationsComponent, Contact, FooterComponent],
+  imports: [HeaderComponent, Hero, About, SkillsComponent, ProjectsComponent, CertificationsComponent, ContactComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
+  standalone: true,
   imports: [],
   templateUrl: './contact.html',
   styleUrl: './contact.scss'
 })
-export class Contact {
+export class ContactComponent {
 
 }
