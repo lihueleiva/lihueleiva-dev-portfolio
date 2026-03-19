71fb54502e5ca8612b20a77ce33faa8228553125
Lihue Leiva <leivalihue@gmail.com>
2026-03-18 21:42:23 -0300

    finish the contact form functionality implementation

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  contactForm!: FormGroup;
+  formStatus: string = '';
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  async sendEmail() {
+    if (this.contactForm.invalid) {
+      this.contactForm.markAllAsTouched();
+      return;
+    }
+
+    this.formStatus = 'Enviando...';
+
+    const templateParams = {
+      name: this.contactForm.value.name,
+      email: this.contactForm.value.email,
+      message: this.contactForm.value.message,
+    };
+
+    try {
+      emailjs.init('ZN2LKQC1W3ZcgrA1b');
+      await emailjs.send('service_hzrvi2m', 'template_e4qxoaf', templateParams);
+      this.formStatus = '¡Mensaje enviado con éxito!';
+      this.contactForm.reset();
+    } catch (error) {
+      console.error('Error al enviar el email:', error);
+      this.formStatus = 'Ocurrió un error al enviar el mensaje. Por favor, intenta de nuevo.';
+    }
+  }
+
+  closeStatusMessage(): void {
+    this.formStatus = '';
+  }
 }
